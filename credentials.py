addba43b34d9c828704a5801d8b441cda25c87e3
Joe Blankenship <joe.blankenship1@gmail.com>
2018-07-09 18:54:32 -0400

    created credentials file template

---
+++ credentials.py
@@ -0,0 +1,8 @@
+"""
+Twitter Manager Credentials
+"""
+
+consumer_key = 'consumer key'
+consumer_secret = 'consumer secrets'
+access_token = 'access token'
+access_token_secret = 'access token secret'
